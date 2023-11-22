98e5870e3cf94c6da8ff4ae53475cd19f0ac5909
Samuel Salazar <samualexekko@gmail.com>
2023-11-21 23:11:24 -0400

    changed: login placeholder

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -41,7 +41,7 @@
         required
         type="text"
         name="name"
-        placeholder="Ej. Pepe"
+        placeholder="Nombre de usuario"
         class="bg-light-700 p-2 rounded text-gray-700 w-full flex"
       />
     </label>
@@ -51,7 +51,7 @@
         required
         type="email"
         name="email"
-        placeholder="Ej. pepe@gmail.com"
+        placeholder="Ej. Ejemplo@gmail.com"
         class="bg-light-700 p-2 rounded text-gray-700 w-full flex"
       />
     </label>
