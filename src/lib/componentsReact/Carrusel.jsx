061ef0876c472a711b1800440c213747e9fdd54c
Samuel Salazar <samualexekko@gmail.com>
2023-11-29 17:40:46 -0400

    bugfixes

--- src/lib/componentsReact/Carrusel.jsx
+++ src/lib/componentsReact/Carrusel.jsx
@@ -1,34 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { pb } from '$lib'
-import './carrusel.css'
+import { pb } from '$lib';
+import './carrusel.css';
 import { PUBLIC_POCKETBASE_URL } from "$env/static/public";
 
-
 const Slide = ({ index, id, title, attachment, description }) => {
-
     return (
         <div className="slide" style={{ backgroundImage: `url(${PUBLIC_POCKETBASE_URL}api/files/by1itjt6pmxxvch/${id}/${attachment})` }}>
             <div className="number">{index < 10 ? `0${index}` : index}</div>
             <div className="body">
                 <div className="name">{title}</div>
                 <div className="headline" dangerouslySetInnerHTML={{ __html: description }}></div>
-
-                <a href="/app" >
+                <a href="/app">
                     <div className="link">Ver más</div>
                 </a>
             </div>
         </div>
     );
 };
+
 const Carrusel = () => {
     const [currentPage, setCurrentPage] = useState(0);
-    const [dataSlides, setDataSlides] = useState([]); // Estado para almacenar los slides
+    const [dataSlides, setDataSlides] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const data = await pb.collection('vulnerabilities').getFullList({ filter: 'severity = "critic" || severity = "high"' });
-                setDataSlides(data.slice(0, 10)); // Almacena los primeros 10 slides
+                setDataSlides(data.slice(0, 10));
             } catch (error) {
                 console.error("Error al cargar datos:", error);
             }
@@ -36,8 +34,8 @@ const Carrusel = () => {
 
         fetchData();
     }, []);
-    const slideCount = dataSlides.length;
 
+    const slideCount = dataSlides.length;
     const slidesPerPage = () => window.innerWidth > 1700 ? 4 : window.innerWidth > 1200 ? 3 : 2;
     const maxPageCount = () => Math.floor(slideCount / slidesPerPage());
 
@@ -52,16 +50,21 @@ const Carrusel = () => {
         const nextButton = document.getElementById('next');
         const prevButton = document.getElementById('prev');
 
-        nextButton.addEventListener('click', nextHandler);
-        prevButton.addEventListener('click', prevHandler);
+        if (nextButton && prevButton) {
+            nextButton.addEventListener('click', nextHandler);
+            prevButton.addEventListener('click', prevHandler);
+        }
 
         return () => {
-            nextButton.removeEventListener('click', nextHandler);
-            prevButton.removeEventListener('click', prevHandler);
+            if (nextButton && prevButton) {
+                nextButton.removeEventListener('click', nextHandler);
+                prevButton.removeEventListener('click', prevHandler);
+            }
         };
-    }, [currentPage]);
+    }, [currentPage, goToPage]);
 
     const carruselRef = useRef(null);
+
     useEffect(() => {
         const observer = new IntersectionObserver(
             (entries) => {
@@ -89,26 +92,18 @@ const Carrusel = () => {
 
     return (
         <div id="hero-slides" style={{ '--page': currentPage }} ref={carruselRef}>
-            <div id="header" >
-                <div className='heading-text neon-text ' style={{ padding: "0px 1.5vw 0px 0px" }}>
-                    Top
-                </div>
-                <div className='heading-text x-safe-text' style={{ padding: "0px 1.5vw 0px 0px" }}>
-                    10
-                </div>
-                <div className='heading-text neon-text'>
-                    Vulnerabilidades
-                </div>
-            </div >
-
-
+            <div id="header">
+                <div className='heading-text neon-text' style={{ padding: "0px 1.5vw 0px 0px" }}>Top</div>
+                <div className='heading-text x-safe-text' style={{ padding: "0px 1.5vw 0px 0px" }}>10</div>
+                <div className='heading-text neon-text'>Vulnerabilidades</div>
+            </div>
             <div id="slides-cont">
                 <div className="button" id="next"></div>
                 <div className="button" id="prev"></div>
                 <div id="slides">
-
                     {dataSlides.map((item, index) => (
                         <Slide
+                            key={item.id}
                             index={index + 1}
                             id={item.id}
                             title={item.title}
@@ -116,7 +111,6 @@ const Carrusel = () => {
                             attachment={item.attachments[0]}
                         />
                     ))}
-
                     <div id="next-catch"></div>
                     <div id="prev-catch"></div>
                 </div>
